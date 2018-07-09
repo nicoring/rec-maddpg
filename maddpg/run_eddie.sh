ec9e126e1108fa2dbb1cf30e2d0643f47df691e4
Nico Ring <ringnico@web.de>
2018-07-09 17:41:02 +0100

    eddie run script

---
+++ maddpg/run_eddie.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#$ -N test              
+#$ -cwd                  
+#$ -l h_rt=00:05:00 
+#$ -l h_vmem=1G
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+ 
+# Load Python
+module load anaconda
+
+source activate thesis
+ 
+"$@"
