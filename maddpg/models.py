61968369e50c9ce9b36e6d8ab7233e93709c1042
Nico Ring <ringnico@web.de>
2018-07-24 22:04:42 +0100

    delete unused import

--- maddpg/models.py
+++ maddpg/models.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import RelaxedOneHotCategorical
-from lstms import LayerNormLSTM
 
 class Clonable:
     def clone(self, requires_grad=False):
